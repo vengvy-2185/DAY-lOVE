75515446b54d73cac221a10252ccc33d56dc6303
vengvy-2185 <ravy218550@gmail.com>
2026-09-01 08:40:49 +0700

    fix import path in auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const prisma = require("../../prisma/schema.prisma");
+const prisma = require('../config/prisma');;
 const { signToken, hashToken } = require("../utils/jwt");
 
 const SESSION_TTL_MS = 7 * 24 * 60 * 60 * 1000; // 7 days
